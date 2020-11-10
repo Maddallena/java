aed19b0c033e0630d05f97b580d24af371f79a79
Magdalena Rajewska <rajewskam63@gamil.com>
2020-11-10 13:49:57 +0100

    ...

---
+++ src/pl/edu/uwm/wmii/magdalenarajewska/kolokwium1/Zadanie2.java
@@ -0,0 +1,20 @@
+package pl.edu.uwm.wmii.magdalenarajewska.kolokwium1;
+
+public class Zadanie2 {
+    public static String delete(String str, char c){
+        int pierwszy = 0;
+        StringBuffer a = new StringBuffer(str);
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) == c){
+                pierwszy =+ 1;
+            }
+            if(pierwszy > 1 && str.charAt(i) == c){
+                a.replace("c", "");
+            }
+        }
+        return a.toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
