b9794ab9b04a9eeb8d1308878ebc8e70c394db38
Magdalena Rajewska <rajewskam63@gamil.com>
2020-11-15 01:42:30 +0100

    ...

--- src/pl/edu/uwm/wmii/magdalenarajewska/laboratorium06/Zadanie1Test.java
+++ src/pl/edu/uwm/wmii/magdalenarajewska/laboratorium06/Zadanie1Test.java
@@ -4,11 +4,14 @@ public class Zadanie1Test {
     public static void main(String[] args) {
         RachunekBankowy saver1 = new RachunekBankowy(2000.00);
         RachunekBankowy saver2 = new RachunekBankowy(3000.00);
-        saver1.rocznaStopaProcentowa = 0.05;
-        saver2.rocznaStopaProcentowa = 0.05;
+        saver1.rocznaStopaProcentowa = 0.04;
         System.out.println("Oszczędności Saver1 wynosiły: " + saver1.obliczMiesieczneOdsteki());
+        saver2.rocznaStopaProcentowa = 0.04;
         System.out.println("Oszczędności Saver2 wynosiły: " + saver2.obliczMiesieczneOdsteki());
-
+        saver1.rocznaStopaProcentowa = 0.05;
+        System.out.println("Oszczędności Saver1 wynosiły: " + saver1.obliczMiesieczneOdsteki());
+        saver2.rocznaStopaProcentowa = 0.05;
+        System.out.println("Oszczędności Saver2 wynosiły: " + saver2.obliczMiesieczneOdsteki());
 
     }
 }
